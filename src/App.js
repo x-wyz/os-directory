ffc1dd0e21becda8b8601f0810015d0247ce8424
Wilson Ye <nextyearwillbebetter1@gmail.com>
2020-11-24 19:05:16 -0800

    base app page - sr1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			user: null
+		}
+
+		this.login = this.login.bind(this);
+		this.logoff = this.logoff.bind(this);
+	}
+
+	login(user){
+		this.setState({user})
+	}
+
+	logoff(){
+		this.setState({user: null})
+	}
+
+	render(){
+		return (
+			<div className="App">
+		      <Switch>
+
+		      </Switch>
+		    </div>
+		)
+	}
 }
 
 export default App;
---
+++ src/pages/searchpage/searchpage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SearchPage extends Component {
+	constructor(props){
+
+	}
+
+	
+}
+
+export default SearchPage;
