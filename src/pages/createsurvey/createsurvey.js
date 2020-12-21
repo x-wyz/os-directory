ec9d5e7c648a78452f362cd09257e38a741af462
Wilson Ye <nextyearwillbebetter1@gmail.com>
2020-12-20 23:58:10 -0800

    create survey page - create form field functions

--- src/pages/createsurvey/createsurvey.js
+++ src/pages/createsurvey/createsurvey.js
@@ -13,11 +13,16 @@ class CreateSurvey extends Component {
 		super(props);
 		this.state = {
 			surveyfields: [
-			]
+			],
+			num: 0
 		}
 		this.createSelectMulti = this.createSelectMulti.bind(this);
 		this.createSelectOne = this.createSelectOne.bind(this);
 		this.createFormField = this.createFormField.bind(this);
+
+		this.selectMulti = this.selectMulti.bind(this);
+		this.selectOne = this.selectOne.bind(this);
+		this.formfield = this.formfield.bind(this);
 
 		this.update = this.update.bind(this);
 
@@ -25,22 +30,80 @@ class CreateSurvey extends Component {
 	}
 
 	createSelectMulti(){
+		const { surveyfields, num } = this.state;
+
+		surveyfields.push({
+			fieldtype: 'checkbox',
+			id: num,
+			title: 'Section header',
+			description: '',
+			options: []
+		})
+
+		this.setState({
+			surveyfields: surveyfields,
+			num: num + 1
+		})
+	}
+
+	selectMulti(){
 		return (
 			<div>
+				selectmulti
 			</div>
 		)
 	}
 
 	createSelectOne(){
+		const { surveyfields, num } = this.state;
+
+		surveyfields.push({
+			fieldtype: 'radio',
+			id: num,
+			title: 'Section header',
+			description: '',
+			options: []
+		})
+
+		this.setState({
+			surveyfields: surveyfields,
+			num: num + 1
+		})
+	}
+
+	selectOne(){
 		return (
 			<div>
+				selectone
 			</div>
 		)
 	}
 
 	createFormField(){
+		const { surveyfields, num } = this.state;
+
+		surveyfields.push({
+			fieldtype: 'formfield',
+			id: num,
+			type: 'text',
+			max: '',
+			min: '',
+			title: 'Section header',
+			description: ''
+		})
+
+		this.setState({
+			surveyfields: surveyfields,
+			num: num + 1
+		})
+
+		console.log('created form field');
+	}
+
+	formfield(){
 		return (
 			<div>
+				formfield
 			</div>
 		)
 	}
@@ -56,14 +119,16 @@ class CreateSurvey extends Component {
 	render(){
 		const { surveyfields } = this.state;
 
+		console.log(this.state);
+
 		return (
 			<div className="createsurvey-page">
 				<div className="create-survey-contents">
 					<h2 className="create-survey-title">Survey builder</h2>
 					<div className="create-survey-controls">
-						<FormControl iconClass="survey-text-icon" title="Form field" description="Text and number fields" icon={faFont} />
-						<FormControl title="Checkbox" description="Select multiple" icon={faCheckSquare} />
-						<FormControl title="Radio" description="Select one" icon={faCheckCircle} />
+						<FormControl iconClass="survey-text-icon" title="Form field" description="Text and number fields" icon={faFont} handler={this.createFormField} />
+						<FormControl title="Checkbox" description="Select multiple" icon={faCheckSquare} handler={this.createSelectMulti} />
+						<FormControl title="Radio" description="Select one" icon={faCheckCircle} handler={this.createSelectOne} />
 					</div>
 				</div>
 			</div>
